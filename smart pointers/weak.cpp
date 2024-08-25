c05a009b03dc234ee6a2869935660f6b79ce542c
Rohit Kumar <34687699+Rohit3649@users.noreply.github.com>
2024-08-25 21:08:06 +0530

    Update weak pointer custom implementation

--- smart pointers/weak.cpp
+++ smart pointers/weak.cpp
@@ -1,58 +1,176 @@
 #include <iostream>
-#include <memory>
-using namespace std;
 
-using Type = int;
-
-template<typename T>
-class MyWeakPtr
-{
-private:
-    shared_ptr<T> sp;
-    bool expired;
+template <typename T>
+class ControlBlock {
 public:
-    MyWeakPtr(const shared_ptr<T>& p) : sp{p}, expired{false} {
-        cout << "copy cons" << endl;
-    }
+    T* ptr;
+    size_t shared_count;
+    size_t weak_count;
 
-    bool hasExpired() const {
-        return expired;
-    }
+    ControlBlock(T* p)
+        : ptr(p), shared_count(1), weak_count(0) {}
 
-    shared_ptr<T> lock() {
-        if (!expired && sp.use_count() > 1) {
-            return sp;
-        }
-        expired = true;
-        return nullptr;
+    ~ControlBlock() {
+        delete ptr;
+        ptr = nullptr;
     }
 };
 
-int main()
-{
-    shared_ptr<Type> sp = make_shared<Type>(10);
-    MyWeakPtr wp(sp);
+template <typename T>
+class SharedPtr;
 
-    if (!wp.hasExpired()) {
-        shared_ptr<Type> tmp = wp.lock();
-        if (tmp) {
-            cout << "Value : " << *tmp << endl;
+template <typename T>
+class WeakPtr {
+public:
+    WeakPtr() : control_block(nullptr) {}
+
+    WeakPtr(const SharedPtr<T>& sharedPtr)
+        : control_block(sharedPtr.control_block) {
+        if (control_block) {
+            ++control_block->weak_count;
         }
-        else {
-            cout << "does not exist anymore" << endl;
+    }
+
+    WeakPtr(const WeakPtr<T>& other)
+        : control_block(other.control_block) {
+        if (control_block) {
+            ++control_block->weak_count;
         }
+    }
+
+    ~WeakPtr() {
+        if (control_block && --control_block->weak_count == 0 && control_block->shared_count == 0) {
+            delete control_block;
+            control_block = nullptr;
+        }
+    }
+
+    WeakPtr& operator=(const WeakPtr<T>& other) {
+        if (this != &other) {
+            release();
+            control_block = other.control_block;
+            if (control_block) {
+                ++control_block->weak_count;
+            }
+        }
+        return *this;
+    }
+
+    SharedPtr<T> lock() const {
+        if (control_block && control_block->shared_count > 0) {
+            return SharedPtr<T>(*this);
+        }
+        return SharedPtr<T>();
+    }
+
+    bool expired() const {
+        return !control_block || control_block->shared_count == 0;
+    }
+
+private:
+    friend class SharedPtr<T>;
+    ControlBlock<T>* control_block;
+
+    void release() {
+        if (control_block && --control_block->weak_count == 0 && control_block->shared_count == 0) {
+            delete control_block;
+            control_block = nullptr;
+        }
+    }
+};
+
+template <typename T>
+class SharedPtr {
+private:
+    //friend class WeakPtr<T>;
+
+    void release() {
+        if (control_block && --control_block->shared_count == 0) {
+            if (control_block->weak_count == 0) {
+                delete control_block;
+                control_block = nullptr;
+            } else {
+                delete control_block->ptr;
+                control_block->ptr = nullptr;
+            }
+        }
+    }
+public:
+    ControlBlock<T>* control_block;
+    SharedPtr(T* p = nullptr)
+        : control_block(p ? new ControlBlock<T>(p) : nullptr) {}
+
+    SharedPtr(const SharedPtr<T>& other)
+        : control_block(other.control_block) {
+        if (control_block) {
+            ++control_block->shared_count;
+        }
+    }
+
+    SharedPtr(const WeakPtr<T>& weakPtr)
+        : control_block(weakPtr.control_block) {
+        if (control_block && control_block->shared_count > 0) {
+            ++control_block->shared_count;
+        } else {
+            control_block = nullptr;
+        }
+    }
+
+    ~SharedPtr() {
+        if (control_block && --control_block->shared_count == 0) {
+            if (control_block->weak_count == 0) {
+                delete control_block;
+                control_block = nullptr;
+            } else {
+                delete control_block->ptr;
+                control_block->ptr = nullptr;
+            }
+        }
+    }
+
+    SharedPtr& operator=(const SharedPtr<T>& other) {
+        if (this != &other) {
+            release();
+            control_block = other.control_block;
+            if (control_block) {
+                ++control_block->shared_count;
+            }
+        }
+        return *this;
+    }
+
+    T* get() const {
+        return control_block ? control_block->ptr : nullptr;
+    }
+
+    T& operator*() const {
+        return *(control_block->ptr);
+    }
+
+    T* operator->() const {
+        return control_block->ptr;
+    }
+
+    operator bool() const {
+        return control_block && control_block->ptr;
+    }
+};
+
+int main() {
+    SharedPtr<int> sp(new int(42));
+    WeakPtr<int> wp = sp;
+
+    if (auto locked = wp.lock()) {
+        std::cout << "Locked value: " << *locked << std::endl;
+    } else {
+        std::cout << "Managed object has expired." << std::endl;
     }
 
     sp = nullptr;
 
-    if (!wp.hasExpired()) {
-        shared_ptr<Type> tmp = wp.lock();
-        if (tmp) {
-            cout << "Value : " << *tmp << endl;
-        }
-        else {
-            cout << "does not exist anymore" << endl;
-        }
+    if (wp.expired()) {
+        std::cout << "Managed object has expired." << std::endl;
     }
+
     return 0;
 }
